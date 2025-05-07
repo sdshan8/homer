96c7276aa468abb6c62229d8aabf9291a6cd6bc9
sdshan8 <sdshan88@protonmail.com>
2025-05-07 12:23:32 +0530

    Boilerplate for BatterryApi

---
+++ src/components/services/BatteryApi.vue
@@ -0,0 +1,23 @@
+<template>
+  <Generic :item="item">
+    <template #icon>
+      <!-- left area containing the icon -->
+    </template>
+    <template #content>
+      <!-- main area containing the title, subtitle, ... -->
+    </template>
+    <template #indicator>
+      <!-- top right area, empty by default -->
+    </template>
+  </Generic>
+</template>
+
+<script>
+
+export default {
+  name: "BatteryApi",
+  props: {
+    item: Object,
+  },
+};
+</script>
